adbb678de78e0064251e7e92070fdb241b6d3b7b
Jonas Vogel <jonas@jonasvogel.name>
2019-11-11 14:40:59 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,15 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "glTFSceneKit",
+    platforms: [
+        // specify each minimum deployment requirement,
+        //otherwise the platform default minimum is used.
+        .iOS(.v10)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
