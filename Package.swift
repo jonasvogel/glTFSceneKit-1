2b4c034751beb13164cf168cde4fc1de3f7cab6e
Jonas Vogel <jonas@jonasvogel.name>
2019-11-11 14:43:47 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "glTFSceneKit",
-            dependencies: []),
+            dependencies: ["Draco"]),
         .testTarget(
             name: "glTFSceneKitTests",
             dependencies: ["glTFSceneKit"]),
